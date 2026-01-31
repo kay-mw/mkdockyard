a2f331550a750916e7fae71a050e7eee925a3467
kay-mw <kiranwhalleymalhotra@gmail.com>
2026-01-31 11:25:17 +0000

    init: proof of concept
    I can't believe it's this simple. This actually works. Wow.

--- src/mkdockyard/main.py
+++ src/mkdockyard/main.py
@@ -1,12 +1,49 @@
+import subprocess
+from pathlib import Path
+
 from mkdocs.config import base
 from mkdocs.config import config_options as c
+from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.plugins import BasePlugin
 
 
+class _Repo(base.Config):
+    """This is a docstring."""
+
+    name = c.Type(str)
+    url = c.Type(str)
+    ref = c.Type(str)
+
+
 class MkdockyardConfig(base.Config):
-    foo = c.Type(dict, default={"a": 1})
+    repos = c.ListOfItems(c.SubConfig(_Repo))
 
 
 class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
-    def on_serve(self, server, config, builder):
-        print("It's working!")
+    def on_config(self, config: MkDocsConfig):
+        cache_dir = Path.home().joinpath(".cache").joinpath("mkdockyard")
+        repos = self.config.repos
+        for repo in repos:
+            url = repo.url
+            ref = repo.ref
+            name = repo.name
+            output_path = cache_dir.joinpath(ref + "-" + name)
+
+            config.plugins["mkdocstrings"].config["handlers"]["python"]["paths"].append(
+                str(output_path)
+            )
+            print(config.plugins["mkdocstrings"].config["handlers"]["python"]["paths"])
+
+            if not output_path.exists():
+                subprocess.run(
+                    [
+                        "git",
+                        "clone",
+                        url,
+                        output_path,
+                        "--depth=1",
+                        f"--revision={ref}",
+                    ],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.STDOUT,
+                )
