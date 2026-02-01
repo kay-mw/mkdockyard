b2452148ada9e62a815a0980977dd10806e17007
kay-mw <kiranwhalleymalhotra@gmail.com>
2026-02-01 11:40:08 +0000

    docs(plugin): add docstrings to all plugin methods and config classes

--- src/mkdockyard/main.py
+++ src/mkdockyard/main.py
@@ -23,25 +23,96 @@ log = logging.getLogger(f"mkdocs.plugins.{__name__}")
 
 @dataclass
 class CloneInformation:
+    """Dataclass describing the information needed to perform a git clone."""
+
     name: str
+    """The name of the directory to clone into."""
     url: str
+    """The `.git` URL of the repo to clone."""
     ref: str
+    """The ref of the repo to clone."""
     hashed_dir: Path
+    """
+    A digested SHA256 hash representing the unique combination of the `url` and `ref`.
+    This serves as the parent directory to the repo name.
+    """
 
 
 class _Repos(base.Config):
+    """Repo configuration attributes."""
+
     name = c.Type(str)
+    """
+    The name of the repo. This parameter determines what name prefix to use when 
+    referencing files from this repo in your documentation.
+
+    Example: 'textual'
+    Reference in documentation: `::: textual.path.to.some.module`
+    """
     url = c.Type(str)
+    """
+    The `.git` URL of the repo. 
+
+    Example: 'https://github.com/Textualize/textual.git'
+    """
     ref = c.Type(str)
+    """
+    The ref of the repo to clone. The ref can be any reference that git can checkout as
+    a revision, such as a branch or commit hash. Commit hashes are recommended, for
+    reproducibility when deploying in CI.
+
+    Example: '501372027f3abc75561881e3803efc34098dabe1'
+    Example: 'main'
+    """
 
 
 class MkdockyardConfig(base.Config):
+    """Mkdockyard configuration options."""
+
     repos = c.ListOfItems(c.SubConfig(_Repos))
+    """
+    A list of one or more repos, each containing the attributes `name`, `url`, and
+    `ref`.
+
+    Example:
+
+        ```yml
+        repos:
+          - name: 'textual'
+            url: 'https://github.com/Textualize/textual.git'
+            ref: '501372027f3abc75561881e3803efc34098dabe1'
+        ```
+    """
     cache_limit_multiplier = c.Type(int, default=2)
+    """
+    An integer representing the multiplier to apply to the cache limit (default: 2).
+
+    To prevent fetching repos on every build, mkdockyard caches repos in your OS's
+    cache directory (`~/.cache/mkdockyard` on Linux). To prevent this cache getting too
+    large, mkdockyard automatically deletes unused directories when the number of
+    unused directories exceeds the cache limit.
+
+    The cache limit is defined relative to the number of repos in the configuration,
+    multiplied by this multiplier.
+
+    This is 2 by default, meaning that **unused repos** will only be pruned when the
+    number of unused repos exceeds the number of **currently configured repos**
+    multiplied by 2.
+    """
 
 
 class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
-    def on_config(self, config: MkDocsConfig):
+    """
+    Clones the git repos defined in the MkdockyardConfig into the OS's cache directory,
+    and adds their paths to mkdocstrings `paths` configuration option.
+    """
+
+    def on_config(self, config: MkDocsConfig) -> None:
+        """
+        Hooks into the mkdocs config loading process, to clone the git repos defined
+        in the MkdockyardConfig into the OS's cache directory, and add their paths to
+        mkdocstrings `paths` configuration option.
+        """
         cache_dir = Path(user_cache_dir("mkdockyard"))
         repos = self.config.repos
 
@@ -110,13 +181,26 @@ class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
             )
             cached_repos = [Path(cache_dir.joinpath(repo)) for repo in cached_repos]
             self.prune_cache(
-                output_paths=configured_repos,
+                configured_repos=configured_repos,
                 cached_repos=cached_repos,
                 cache_dir=cache_dir,
             )
 
     @staticmethod
     def get_git_version() -> tuple[int, int]:
+        """
+        Gets the Git version of the machine running mkdocs. This is necessary to
+        identify whether the machine has a git version >= 2.49.
+
+        Git version 2.49 added the `--revision` flag to `git clone`, which allows
+        users to fetch a specific ref with one command. Detecting versions that support
+        this flag therefore allows mkdockyard to use this superior approach if it's
+        available.
+
+        Returns:
+            A `tuple[int, int]`, where the first item in the tuple is the major version,
+            and the second item is the minor version.
+        """
         version_string = subprocess.run(
             ["git", "--version"], check=True, capture_output=True, text=True
         )
@@ -131,6 +215,17 @@ class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
     def build_clone_information(
         repos: list[_Repos], cache_dir: Path
     ) -> list[CloneInformation]:
+        """
+        Builds the information necessary to clone a git repo into the OS's cache
+        directory (`CloneInformation`), for each repo in the list.
+
+        Parameters:
+            repos: A list of repos, as defined in the mkdockyard configuration.
+            cache_dir: The `Path` to mkdockyard's cache directory.
+
+        Returns:
+            `list[CloneInformation]`.
+        """
         clone_information: list[CloneInformation] = []
         for repo in repos:
             name = repo.name
@@ -145,10 +240,18 @@ class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
         return clone_information
 
     @staticmethod
-    def subprocess_run_wrapper(args: list[str], output_path: Path) -> None:
+    def subprocess_run_wrapper(args: list[str], cwd: Path) -> None:
+        """
+        Wrapper around subprocess.run, to allow for reduced repetition of paramaters
+        common across git commands run in `Mkdockyard.clone_git_repo`.
+
+        Parameters:
+            args: The list of arguments to pass to `subprocess.run`
+            cwd: The directory to run the subprocess in.
+        """
         subprocess.run(
             args,
-            cwd=output_path,
+            cwd=cwd,
             check=True,
             capture_output=True,
             text=True,
@@ -158,6 +261,22 @@ class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
     def clone_git_repo(
         git_supports_revision: bool, url: str, ref: str, output_path: Path
     ) -> None:
+        """
+        Clones the git repo for the specified `url` and `ref` into the provided
+        `output_path`.
+
+        Clones the specified ref using the `--revision` flag, git versions (>=2.49)
+        that support it, as this approach is far simpler. Otherwise, uses the more
+        complex legacy approach of initialising an empty repo at `output_path`, adding
+        the URL as a remote, fetching the specific ref, and checking out the `HEAD`.
+
+        Parameters:
+            git_supports_revision: A `bool` indicating whether the machine running
+            mkdocs supports the `git clone` `--revision` flag.
+            url: The `.git` URL of the repo to clone.
+            ref: The ref to checkout in the target repo.
+            output_path: The path to clone the repo into.
+        """
         if git_supports_revision:
             subprocess.run(
                 [
@@ -175,18 +294,16 @@ class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
             return
 
         os.makedirs(output_path)
+        MkdockyardPlugin.subprocess_run_wrapper(args=["git", "init"], cwd=output_path)
         MkdockyardPlugin.subprocess_run_wrapper(
-            args=["git", "init"], output_path=output_path
-        )
-        MkdockyardPlugin.subprocess_run_wrapper(
-            args=["git", "remote", "add", "origin", url], output_path=output_path
+            args=["git", "remote", "add", "origin", url], cwd=output_path
         )
         MkdockyardPlugin.subprocess_run_wrapper(
             args=["git", "fetch", "--depth", "1", "origin", ref],
-            output_path=output_path,
+            cwd=output_path,
         )
         MkdockyardPlugin.subprocess_run_wrapper(
-            args=["git", "checkout", "FETCH_HEAD"], output_path=output_path
+            args=["git", "checkout", "FETCH_HEAD"], cwd=output_path
         )
 
     @staticmethod
@@ -197,6 +314,29 @@ class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
         name: str,
         git_supports_revision: bool,
     ) -> bool:
+        """
+        The core logic for the mkdockyard plugin. This clones the git repo if it
+        doesn't exist, renames it if the repo does exist but changed name, or reuses it
+        if none of the ref/url/name have changed.
+
+        The `bool` return value indicates whether the repo was cloned. This is purely
+        so cloning can be logged as it occurs, rather than before it occurs, in the
+        main `Mkdockyard.on_config` method. This is necessary as cloning is done using
+        `concurrent.futures`.
+
+        Parameters:
+            url: The `.git` URL of the repo to clone.
+            ref: The ref to checkout in the target repo.
+            hashed_dir: A digested SHA256 hash representing the unique combination of
+            the `url` and `ref`. This serves as the parent directory to the repo name.
+            name: The name of the directory to clone into.
+            git_supports_revision: A `bool` indicating whether the machine running
+            mkdocs supports the `git clone` `--revision` flag.
+
+        Returns:
+            A `bool` indicating whether the repo was cloned (`True`) or renamed/reused
+            (`False`).
+        """
         output_path = hashed_dir.joinpath(name)
         if not hashed_dir.exists():
             MkdockyardPlugin.clone_git_repo(
@@ -220,10 +360,19 @@ class MkdockyardPlugin(BasePlugin[MkdockyardConfig]):
 
     @staticmethod
     def prune_cache(
-        output_paths: list[Path], cached_repos: list[Path], cache_dir: Path
+        configured_repos: list[Path], cached_repos: list[Path], cache_dir: Path
     ) -> None:
+        """
+        Removes repos currently not in the configuration from the cache.
+
+        Parameters:
+            configured_repos: A `list[Path]` of repos currently in the mkdockyard
+            configuration.
+            cached_repos: A `list[Path]` of all repos currently in the cache.
+            cache_dir: The `Path` to mkdockyard's cache directory.
+        """
         for repo in cached_repos:
-            if repo in output_paths:
+            if repo in configured_repos:
                 continue
 
             if not repo.is_relative_to(cache_dir):
